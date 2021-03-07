404dc6259c4c2f278f16073e73b9fcfa3a07026d
fots <60556553+fotscode@users.noreply.github.com>
2021-03-07 20:34:53 -0300

    initial commit

---
+++ src/components/QuoteGenerator.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import Async from 'react-async';
+
+var quotesArray;
+
+function randomizeInteger(min, max) {
+  	if(max == null) {
+    	max = (min == null ? Number.MAX_SAFE_INTEGER : min);
+      	min = 0;
+    }
+
+    min = Math.ceil(min);  // inclusive min
+    max = Math.floor(max); // exclusive max
+
+  	if(min > max - 1) {
+    	throw new Error("Incorrect arguments.");
+    }
+
+    return min + Math.floor((max - min + 1) * Math.random());
+}
+
+function changeColor() {
+  let hue=randomizeInteger(0,360);
+  let lightness=randomizeInteger(75,100);
+  let randomColor=`hsl(${hue},100%,${lightness}%)`
+  document.body.style.background = randomColor;
+  let button=document.getElementById("new-quote");
+  let twitter=document.getElementById("tweet-quote");
+  let text = document.getElementById("text");
+  let author= document.getElementById("author");
+  button.style.backgroundColor=randomColor;
+  twitter.style.backgroundColor=randomColor;
+  text.style.color=randomColor;
+  author.style.color=randomColor;
+  return false;
+}
+
+
+class QuoteGenerator extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={
+			quote:{quote:'Loading',author:'Loading'}
+    }
+    this.handleNewQuote=this.handleNewQuote.bind(this);
+  }
+
+	componentDidMount(){
+		let req = new XMLHttpRequest;
+		req.onload = ((e)=>{
+			quotesArray= JSON.parse(e.target.response);
+			quotesArray=quotesArray.quotes;
+   	  this.setState(()=>({quote:quotesArray[randomizeInteger(0,101)]}));
+		});
+		req.open("get","https://gist.githubusercontent.com/nasrulhazim/54b659e43b1035215cd0ba1d4577ee80/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json",true)
+		req.send();
+	}
+
+  handleNewQuote(){
+    changeColor();
+    this.setState(()=>({quote:quotesArray[randomizeInteger(0,101)]}));
+  }
+
+  render(){
+    return(
+      <div id="quote-box">
+       <q id="text">{this.state.quote.quote}</q>
+        <address id="author">- {this.state.quote.author}</address>
+         <a href="twitter.com/intent/tweet" target="_blank" className="btn" id="tweet-quote"><i class="fa fa-twitter"></i></a>
+       <button className="btn" id="new-quote" onClick={this.handleNewQuote}>New quote</button>
+      </div>
+    );
+  }
+}
+
+export default QuoteGenerator;
